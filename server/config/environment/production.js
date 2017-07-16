e11c7bdab365eb2d943fa3994dd71fc5b1f4ed2a
fengzhyuan <feng.zhyuan@gmail.com>
2017-07-16 14:56:56 -0700

    update configure

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -5,23 +5,23 @@
 // =================================
 module.exports = {
   // Server IP
-  ip: process.env.OPENSHIFT_NODEJS_IP
-    || process.env.ip
+  ip: process.env.ip
     || undefined,
 
   // Server port
-  port: process.env.OPENSHIFT_NODEJS_PORT
-    || process.env.PORT
+  port: process.env.PORT
     || 8080,
 
   sequelize: {
     uri: process.env.SEQUELIZE_URI
-      || 'postgres://postgres:postgres@localhost:5432/dist.pean',
+      || 'postgres://spfqiwhdfdvxeh:76538898867857ce6b3ca8772c8773fc153280c2120eb11d7813fa56fffd7b04@ec2-107-20-226-93.compute-1.amazonaws.com:5432/d7cus5bcoi6708',
     options: {
-      logging: false,
-      define: {
-        timestamps: false
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: true
       }
-    }
-  }
+    },
+    ssl:true
+  },
+  seedDB: false
 };
